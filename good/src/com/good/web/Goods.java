a0ff90b1d966f01704664970273678568353b48b
lijiajun <lijianjun@zhubajie.com>
2014-11-17 12:32:42 +0800

    fdsaf

---
+++ good/src/com/good/web/Goods.java
@@ -0,0 +1,15 @@
+package com.good.web;
+
+/**
+ * ClassName:Goods <br/>
+ * Description: TODO <br/>
+ * Date: 2014年11月17日 下午12:06:26 <br/>
+ *
+ * @author Jim
+ * @version 1.0
+ * @since JDK 1.6 
+ */
+public class Goods {
+
+}
+
